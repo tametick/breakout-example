fb6b91014a5073b0a740110bdbfe5478c60c2594
Ido Yehieli <ido.yehieli@gmail.com>
2020-05-14 12:59:54 +0200

    bouncing wall with physics material instead of script

--- Assets/Scripts/Wall.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-public class Wall : MonoBehaviour {
-	public Vector3 bounceDirection;
-
-	void OnCollisionEnter(Collision collision) {
-		collision.rigidbody.AddForce(bounceDirection, ForceMode.VelocityChange);
-	}
-}
