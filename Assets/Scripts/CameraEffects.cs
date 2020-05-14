366bb95fb48f17e9186ea180f67f0d84e0ae105c
Ido Yehieli <ido.yehieli@gmail.com>
2020-05-14 14:11:07 +0200

    removed old stuff

--- Assets/Scripts/CameraEffects.cs
+++
@@ -1,9 +0,0 @@
-﻿using DG.Tweening;
-using UnityEngine;
-public class CameraEffects : MonoBehaviour {
-
-	void Start() {
-		transform.DOMoveY(1, 1).SetEase(Ease.OutBounce);
-	}
-
-}
--- Assets/Scripts/FallIntoPlace.cs
+++ Assets/Scripts/FallIntoPlace.cs
@@ -5,7 +5,7 @@ public class FallIntoPlace : MonoBehaviour {
 	void Start() {
 		float targetY = transform.position.y;
 		transform.Translate(new Vector3(0, 10, 0));
-		transform.DOMoveY(targetY, Random.RandomRange(0.5f, 1)).SetEase(Ease.OutBounce);
+		transform.DOMoveY(targetY, Random.Range(0.5f, 1)).SetEase(Ease.OutBounce);
 	}
 
 }
